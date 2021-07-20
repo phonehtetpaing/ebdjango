0e2f2f5c60d69a17b15e307d641883f967c406c4
Stephan <phonehtetpaing1221@gmail.com>
2021-07-20 20:30:52 +0630

    revert migrations

--- apps/core/migrations/0067_auto_20180725_1607.py
+++ apps/core/migrations/0067_auto_20180725_1607.py
@@ -11,43 +11,43 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Todo',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('end_user_reply', models.CharField(max_length=2048, null=True, verbose_name='reply text or payload')),
-                ('title', models.TextField(null=True, verbose_name='title')),
-                ('memo', models.TextField(null=True, verbose_name='memo')),
-                ('regist_dt', models.DateTimeField(auto_now_add=True, null=True, verbose_name='regist datetime')),
-                ('update_dt', models.DateTimeField(auto_now=True, null=True, verbose_name='update datetime')),
-                ('is_delete', models.BooleanField(default=0, verbose_name='delete flg')),
-                ('end_user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_end_user', to='core.EndUser', verbose_name='end_user')),
-                ('todo_action_status', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_todo_action_status', to='core.TodoActionStatus', verbose_name='todo_action_status')),
-                ('vendor_branch', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_vendor_branch', to='core.VendorBranch', verbose_name='vendor_branch')),
-            ],
-            options={
-                'verbose_name': 'Todo',
-                'permissions': (),
-            },
-        ),
-        migrations.AlterField(
-            model_name='automessagecontroller',
-            name='auto_message_trigger',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagecontroller_auto_message_trigger', to='core.AutoMessageTrigger', verbose_name='auto_message_trigger'),
-        ),
-        migrations.AlterField(
-            model_name='automessagehistory',
-            name='auto_message_condition',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagehistory_auto_message_condition', to='core.AutoMessageCondition', verbose_name='auto_message_condition'),
-        ),
-        migrations.AlterField(
-            model_name='automessagehistory',
-            name='auto_message_trigger',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagehistory_auto_message_trigger', to='core.AutoMessageTrigger', verbose_name='auto_message_trigger'),
-        ),
-        migrations.AlterField(
-            model_name='automessagetrigger',
-            name='auto_message_condition',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagetrigger_auto_message_condition', to='core.AutoMessageCondition', verbose_name='auto_message_condition'),
-        ),
+#         migrations.CreateModel(
+#             name='Todo',
+#             fields=[
+#                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+#                 ('end_user_reply', models.CharField(max_length=2048, null=True, verbose_name='reply text or payload')),
+#                 ('title', models.TextField(null=True, verbose_name='title')),
+#                 ('memo', models.TextField(null=True, verbose_name='memo')),
+#                 ('regist_dt', models.DateTimeField(auto_now_add=True, null=True, verbose_name='regist datetime')),
+#                 ('update_dt', models.DateTimeField(auto_now=True, null=True, verbose_name='update datetime')),
+#                 ('is_delete', models.BooleanField(default=0, verbose_name='delete flg')),
+#                 ('end_user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_end_user', to='core.EndUser', verbose_name='end_user')),
+#                 ('todo_action_status', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_todo_action_status', to='core.TodoActionStatus', verbose_name='todo_action_status')),
+#                 ('vendor_branch', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='todo_vendor_branch', to='core.VendorBranch', verbose_name='vendor_branch')),
+#             ],
+#             options={
+#                 'verbose_name': 'Todo',
+#                 'permissions': (),
+#             },
+#         ),
+#         migrations.AlterField(
+#             model_name='automessagecontroller',
+#             name='auto_message_trigger',
+#             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagecontroller_auto_message_trigger', to='core.AutoMessageTrigger', verbose_name='auto_message_trigger'),
+#         ),
+#         migrations.AlterField(
+#             model_name='automessagehistory',
+#             name='auto_message_condition',
+#             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagehistory_auto_message_condition', to='core.AutoMessageCondition', verbose_name='auto_message_condition'),
+#         ),
+#         migrations.AlterField(
+#             model_name='automessagehistory',
+#             name='auto_message_trigger',
+#             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagehistory_auto_message_trigger', to='core.AutoMessageTrigger', verbose_name='auto_message_trigger'),
+#         ),
+#         migrations.AlterField(
+#             model_name='automessagetrigger',
+#             name='auto_message_condition',
+#             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='automessagetrigger_auto_message_condition', to='core.AutoMessageCondition', verbose_name='auto_message_condition'),
+#         ),
     ]
