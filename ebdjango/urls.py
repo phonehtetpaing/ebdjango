b5f2a7dca3856952036428da2ed078ef7fced52a
Stephan <phonehtetpaing1221@gmail.com>
2021-07-20 20:02:42 +0630

    updating create superuser

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('hello/<name>', home.hello),
+    path('savetodb/<name>', home.saveToDb)
 ]
